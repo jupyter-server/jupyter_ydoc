e85802f864cb569daca72c4f855a9ed3c5f02920
GitHub Action <41898282+github-actions[bot]@users.noreply.github.com>
2022-06-30 16:41:34 +0000

    Publish 0.1.12
    SHA256 hashes:
    
    jupyter_ydoc-0.1.12-py2.py3-none-any.whl: 04c0963c45a67a6c9f40f24004ec665084a9a5f4e8ffa8e268d21b1f785680eb
    
    jupyter_ydoc-0.1.12.tar.gz: 91ce990611d6a7e29985c6c749736dda219ef9306ee8bb5ecb21e85d40f7e085

--- jupyter_ydoc/__init__.py
+++ jupyter_ydoc/__init__.py
@@ -4,4 +4,4 @@ from .ydoc import YFile, YNotebook  # noqa
 
 ydocs = {ep.name: ep.load() for ep in pkg_resources.iter_entry_points(group="jupyter_ydoc")}
 
-__version__ = "0.1.11"
+__version__ = "0.1.12"
