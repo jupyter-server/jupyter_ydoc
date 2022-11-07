a7ed31c7e7bf0a4a8ab22a39ef90867db455794c
Frédéric Collonval <fcollonval@gmail.com>
2022-11-07 18:13:39 +0100

    Fix jest configuration

--- javascript/jest.config.cjs
+++ javascript/jest.config.cjs
@@ -3,31 +3,10 @@
  * Distributed under the terms of the Modified BSD License.
  */
 
-const path = require('path');
 const esModules = ['lib0', 'y-protocols', 'y-websocket', 'yjs'].join('|');
 
 module.exports = {
-  preset: 'ts-jest/presets/js-with-babel',
-  testTimeout: 10000,
-  testPathIgnorePatterns: ['/lib/', '/node_modules/'],
-  moduleFileExtensions: [
-    'ts',
-    'tsx',
-    'js',
-    'jsx',
-    'json',
-    'node',
-    'mjs',
-    'cjs'
-  ],
-  transformIgnorePatterns: [`/node_modules/(?!${esModules}).+`],
-  reporters: ['default'],
-  coverageReporters: ['json', 'lcov', 'text', 'html'],
-  coverageDirectory: path.join(__dirname, 'coverage'),
-  testRegex: '/test/.*.spec.ts[x]?$',
-  globals: {
-    'ts-jest': {
-      tsconfig: `./tsconfig.test.json`
-    }
-  }
+  testEnvironment: 'node',
+  testRegex: 'lib/test/.*.spec.js[x]?$',
+
 };
