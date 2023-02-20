2333c039f5f98a6ae47920519528c0bce8c85142
David Brochart <david.brochart@gmail.com>
2023-02-20 10:40:19 +0100

    Update with ypy v0.6.0

--- jupyter_ydoc/ynotebook.py
+++ jupyter_ydoc/ynotebook.py
@@ -2,6 +2,7 @@
 # Distributed under the terms of the Modified BSD License.
 
 import copy
+import json
 from functools import partial
 from typing import Any, Callable, Dict, Optional
 from uuid import uuid4
@@ -87,8 +88,8 @@ class YNotebook(YBaseDoc):
         :return: A cell.
         :rtype: Dict[str, Any]
         """
-        meta = self._ymeta.to_json()
-        cell = self._ycells[index].to_json()
+        meta = json.loads(self._ymeta.to_json())
+        cell = json.loads(self._ycells[index].to_json())
         cast_all(cell, float, int)  # cells coming from Yjs have e.g. execution_count as float
         if "id" in cell and meta["nbformat"] == 4 and meta["nbformat_minor"] <= 4:
             # strip cell IDs if we have notebook format 4.0-4.4
@@ -191,7 +192,7 @@ class YNotebook(YBaseDoc):
         :return: Document's content.
         :rtype: Dict
         """
-        meta = self._ymeta.to_json()
+        meta = json.loads(self._ymeta.to_json())
         cast_all(meta, float, int)  # notebook coming from Yjs has e.g. nbformat as float
         cells = []
         for i in range(len(self._ycells)):
