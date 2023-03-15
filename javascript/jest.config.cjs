abe42f1e7e867e9701253de1ad0e2748144acb0f
Frédéric Collonval <fcollonval@gmail.com>
2023-03-15 15:29:23 +0100

    Clean jest config

--- javascript/jest.config.cjs
+++ javascript/jest.config.cjs
@@ -3,10 +3,7 @@
  * Distributed under the terms of the Modified BSD License.
  */
 
-const esModules = ['lib0', 'y-protocols', 'y-websocket', 'yjs'].join('|');
-
 module.exports = {
   testEnvironment: 'node',
   testRegex: 'lib/test/.*.spec.js[x]?$',
-
 };
