91c3b9a007e2b7eb3d22f4d53423caabef55fda0
David Brochart <david.brochart@gmail.com>
2023-11-15 10:32:07 +0100

    Add cell execution_state

--- jupyter_ydoc/ynotebook.py
+++ jupyter_ydoc/ynotebook.py
@@ -38,6 +38,7 @@ class YNotebook(YBaseDoc):
                     "cell_type": str,
                     "source": YText,
                     "metadata": YMap,
+                    "execution_state": str,
                     "execution_count": Int | None,
                     "outputs": [] | None,
                     "attachments": {} | None
@@ -100,6 +101,7 @@ class YNotebook(YBaseDoc):
         """
         meta = self._ymeta.to_py()
         cell = self._ycells[index].to_py()
+        cell.pop("execution_state", None)
         cast_all(cell, float, int)  # cells coming from Yjs have e.g. execution_count as float
         if "id" in cell and meta["nbformat"] == 4 and meta["nbformat_minor"] <= 4:
             # strip cell IDs if we have notebook format 4.0-4.4
@@ -164,6 +166,7 @@ class YNotebook(YBaseDoc):
                     output["text"] = Array(output.get("text", []))
                 outputs[idx] = Map(output)
             cell["outputs"] = Array(outputs)
+            cell["execution_state"] = "idle"
 
         return Map(cell)
 
