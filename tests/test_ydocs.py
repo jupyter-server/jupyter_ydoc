eee08b44f28a2abd910de41e08e805ca5a1b54f6
David Brochart <david.brochart@gmail.com>
2024-07-10 11:38:30 +0200

    Remove YBlob test

--- tests/test_ydocs.py
+++ tests/test_ydocs.py
@@ -1,27 +1,7 @@
 # Copyright (c) Jupyter Development Team.
 # Distributed under the terms of the Modified BSD License.
 
-from jupyter_ydoc import YBlob, YNotebook
-
-
-def test_yblob():
-    yblob = YBlob()
-    assert yblob.get() == b""
-    yblob.set(b"012")
-    assert yblob.get() == b"012"
-    changes = []
-
-    def callback(topic, event):
-        print(topic, event)
-        changes.append((topic, event))
-
-    yblob.observe(callback)
-    yblob.set(b"345")
-    assert len(changes) == 1
-    topic, event = changes[0]
-    assert topic == "source"
-    assert event.keys["bytes"]["oldValue"] == b"012"
-    assert event.keys["bytes"]["newValue"] == b"345"
+from jupyter_ydoc import YNotebook
 
 
 def test_ynotebook_undo_manager():
