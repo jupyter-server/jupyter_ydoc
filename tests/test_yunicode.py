b2a83236948934577fe08449243442c2ca83546d
krassowski <5832902+krassowski@users.noreply.github.com>
2025-11-16 21:36:27 +0000

    Add failing test for YUnicode reload

---
+++ tests/test_yunicode.py
@@ -0,0 +1,21 @@
+from jupyter_ydoc import YUnicode
+
+
+def test_set_no_op_if_unchaged():
+    text = YUnicode()
+    text.set("test content")
+
+    changes = []
+
+    def record_changes(topic, event):
+        changes.append((topic, event))
+
+    text.observe(record_changes)
+
+    model = text.get()
+
+    # Call set with identical text
+    text.set(model)
+
+    # No changes should be observed at all
+    assert changes == []
