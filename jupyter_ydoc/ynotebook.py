283cd46eb1a10bb44b6a5c93a5c314fa94063559
krassowski <5832902+krassowski@users.noreply.github.com>
2025-11-21 12:05:36 +0000

    Allow reload of outputs array if none of the outputs is a stream

--- jupyter_ydoc/ynotebook.py
+++ jupyter_ydoc/ynotebook.py
@@ -345,9 +345,14 @@ class YNotebook(YBaseDoc):
         for key in shared_keys:
             if old_cell[key] != new_cell[key]:
                 value = new_cell[key]
-                if key == "outputs" and value:
-                    # outputs require complex handling - some have Text type nested;
-                    # for now skip creating them; clearing all outputs is fine
+                if (
+                    key == "outputs"
+                    and value
+                    and any(output.get("output_type") == "stream" for output in value)
+                ):
+                    # Outputs with stream require complex handling as they have
+                    # the Text type nested inside; for now skip creating them.
+                    # Clearing all outputs is fine.
                     return False
 
                 if key in _CELL_KEY_TYPE_MAP:
